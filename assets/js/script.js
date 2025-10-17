c4700a9d7c06844cc6afc8dc63395508697b6920
Michela <micheladellagatta1@gmail.com>
2025-10-17 19:09:03 +0200

    On click, change background color of the cell to enhace selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,9 @@ for (let i=0; i<10; i++) {
   for (let j=0; j<10; j++) {
     const cell = document.createElement('div');
     cell.className = 'grid-cell';
+    cell.onclick = () => cell.classList.add('bg-primary');
     row.appendChild(cell);
+
   }
   grid.appendChild(row);
 }
